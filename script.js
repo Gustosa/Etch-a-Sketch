1c617bc4e4673810a1616ae5ddf4d780f0cf6c74
Gustosa <gustavomartins5820@gmail.com>
2024-05-21 20:39:04 -0300

    Add hovering trail effect

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ for (let i = 0; i < 16; i++) {
     for (let q = 0; q < 16; q++) {
         const square = document.createElement("div")
         square.setAttribute("class", "square")
+        square.addEventListener("mouseout", (event) => {
+            if (!event.target.classList.contains("hover"))
+                event.target.classList.add("hover")
+        })
 
         column.appendChild(square)
     }
