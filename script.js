bba5de63ff1042ead67003c08bbdd9605d69ba6d
Gustosa <gustavomartins5820@gmail.com>
2024-05-21 21:44:41 -0300

    Add extra features: random square colors and fading upon hovering

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const new_grid_button = document.querySelector("#new-grid button")
 const container = document.querySelector("#container")
 
+function randomizeRBG() {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    const rgb = "rgb(" + red + ", " + green + ", " + blue + ")"
+
+    return rgb
+}
+
 function createGrid(squares) {
     container.innerHTML = ""
 
@@ -11,9 +21,12 @@ function createGrid(squares) {
         for (let q = 0; q < squares; q++) {
             const square = document.createElement("div")
             square.setAttribute("class", "square")
-            square.addEventListener("mouseout", (event) => {
-                if (!event.target.classList.contains("hover"))
-                    event.target.classList.add("hover")
+            square.style = `background-color: ${randomizeRBG()}; opacity: 1`
+
+            square.addEventListener("mouseenter", (event) => {
+                if (square.style.opacity > 0) {
+                    event.target.style.opacity -= 0.1
+                }
             })
 
             column.appendChild(square)
@@ -25,7 +38,6 @@ function createGrid(squares) {
 
 new_grid_button.addEventListener("click", () => {
     const squares = parseInt(prompt("Please, enter the size (between 1 and 100) of the new grid:"))
-
     createGrid(squares)
 })
 
