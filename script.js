67bfb32567db18d614a4fd104f98e6b4ae85eedb
Gustosa <gustavomartins5820@gmail.com>
2024-05-21 21:13:43 -0300

    Add an option to create a new grid

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+const new_grid_button = document.querySelector("#new-grid button")
 const container = document.querySelector("#container")
 
+function createGrid(squares) {
+    container.innerHTML = ""
 
-for (let i = 0; i < 16; i++) {
-    const column = document.createElement("div")
-    column.setAttribute("class", "column")
+    for (let i = 0; i < squares; i++) {
+        const column = document.createElement("div")
+        column.setAttribute("class", "column")
 
-    for (let q = 0; q < 16; q++) {
-        const square = document.createElement("div")
-        square.setAttribute("class", "square")
-        square.addEventListener("mouseout", (event) => {
-            if (!event.target.classList.contains("hover"))
-                event.target.classList.add("hover")
-        })
+        for (let q = 0; q < squares; q++) {
+            const square = document.createElement("div")
+            square.setAttribute("class", "square")
+            square.addEventListener("mouseout", (event) => {
+                if (!event.target.classList.contains("hover"))
+                    event.target.classList.add("hover")
+            })
 
-        column.appendChild(square)
+            column.appendChild(square)
+        }
+
+        container.appendChild(column)
     }
-
-    container.appendChild(column)
 }
+
+new_grid_button.addEventListener("click", () => {
+    const squares = parseInt(prompt("Please, enter the size (between 1 and 100) of the new grid:"))
+
+    createGrid(squares)
+})
+
+createGrid(16)
